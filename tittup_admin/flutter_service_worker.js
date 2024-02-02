d06a0cf91ed47233de3ba15e1c9ef69c85f4757e
kilho <kiruya80@gmail.com>
2024-02-03 02:24:57 +0900

    도메인 https 설정

--- tittup_admin/flutter_service_worker.js
+++ tittup_admin/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "e5f9301b602ed644d9ea325ee00191d6",
-"index.html": "76214b3106f15e11015e7cd652602a16",
-"/": "76214b3106f15e11015e7cd652602a16",
-"main.dart.js": "8cb8640dc0db5cdda9d80aa6bd36af64",
+"index.html": "96b7066fa7bc3973a00d6ffc7c7f39e9",
+"/": "96b7066fa7bc3973a00d6ffc7c7f39e9",
+"main.dart.js": "730a86f49dd7bfcba1402cbc5aa57402",
 "404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
