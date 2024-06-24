5d198635a967c2274beac198e14845a68bdc77f7
KILHO <kiruya80@gmail.com>
2024-06-24 17:44:29 +0900

    test web 추가

--- flutter_sample/flutter_service_worker.js
+++ flutter_sample/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "067bf5e03989b7408e7e7139aeb8113e",
+const RESOURCES = {"flutter_bootstrap.js": "39fbeb85e2276786878ba0777cbe1ce1",
 "version.json": "5bcc5d2b189ef6c2a8dc3115304251d5",
-"index.html": "6412e81d52294925727af3bac2170a4a",
-"/": "6412e81d52294925727af3bac2170a4a",
+"index.html": "ebadfd84cc8366f3f7c4b596eff9e956",
+"/": "ebadfd84cc8366f3f7c4b596eff9e956",
 "main.dart.js": "76236449c4dc443b9e3d0c71f7d11030",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
